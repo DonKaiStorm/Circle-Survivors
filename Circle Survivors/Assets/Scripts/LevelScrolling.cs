1b0dfa30f032a31b6c4cbea94dbde8ecb8e398af
Jack Stormont <stormont.jack@gmail.com>
2023-01-31 13:53:31 -0500

    Infinite Level System
    It's still a bit jank and contains unfinished assets, but overall...it works. It just needs to be refined a bit more later on.

---
+++ Circle Survivors/Assets/Scripts/LevelScrolling.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScrolling : MonoBehaviour
+{
+
+    [SerializeField] Transform playerTransform;
+    Vector2Int currentTilePosition = new Vector2Int(0,0);
+	[SerializeField] Vector2Int playerTilePosition;
+	Vector2Int onTileGridPlayerPosition;
+
+
+	[SerializeField] float tileSize = 2f;
+    GameObject[,] terrainTiles;
+
+	[SerializeField] int terrainTileHorizontalCount;
+	[SerializeField] int terrainTileVerticalCount;
+
+	[SerializeField] int fovHeight = 3;
+	[SerializeField] int fovWidth = 3;
+
+	public void Add(GameObject gameObject, Vector2Int tilePosition)
+	{
+		terrainTiles[tilePosition.x, tilePosition.y] = gameObject;
+	}
+
+	private void Awake()
+	{
+		terrainTiles = new GameObject[terrainTileHorizontalCount, terrainTileVerticalCount];
+	}
+	private void Update()
+	{
+		playerTilePosition.x = (int)(playerTransform.position.x / tileSize);
+		playerTilePosition.y = (int)(playerTransform.position.y / tileSize);
+
+		playerTilePosition.x -= playerTransform.position.x < 0 ? 1 : 0;
+		playerTilePosition.y -= playerTransform.position.y < 0 ? 1 : 0;
+		if (currentTilePosition != playerTilePosition)
+		{
+			currentTilePosition = playerTilePosition;
+
+			onTileGridPlayerPosition.x = CalculateWrapPosition(onTileGridPlayerPosition.x, true);
+			onTileGridPlayerPosition.y = CalculateWrapPosition(onTileGridPlayerPosition.y, false);
+			UpdateScreen();
+		}
+	}
+
+	private int CalculateWrapPosition(float currentValue, bool horizontal)
+	{
+		if (horizontal)
+		{
+			if (currentValue >= 0)
+			{
+				currentValue = currentValue % terrainTileHorizontalCount;
+			}
+			else
+			{
+				currentValue += 1;
+				currentValue = terrainTileHorizontalCount - 1 + currentValue % terrainTileHorizontalCount;
+			}
+		}
+		else
+		{
+			if (currentValue >= 0)
+			{
+				currentValue = currentValue % terrainTileVerticalCount;
+			}
+			else
+			{
+				currentValue += 1;
+				currentValue = terrainTileVerticalCount - 1 + currentValue % terrainTileVerticalCount;
+			}
+		}
+
+		return (int)currentValue;
+	}
+
+	private void UpdateScreen()
+	{
+		for (int pov_x = -(fovWidth / 2); pov_x <= fovWidth / 2; pov_x++)
+		{
+			for (int pov_y = -(fovHeight / 2); pov_y <= fovHeight / 2; pov_y++)
+			{
+				int tileUpdate_x = CalculateWrapPosition(playerTilePosition.x + pov_x, true);
+				int tileUpdate_y = CalculateWrapPosition(playerTilePosition.y + pov_y, false);
+
+				GameObject tile = terrainTiles[tileUpdate_x, tileUpdate_y];
+				tile.transform.position = CalculateTilePosition(playerTilePosition.x + pov_x, playerTilePosition.y + pov_y);
+			}
+		}
+	}
+
+	private Vector3 CalculateTilePosition(int x, int y)
+	{
+		return new Vector3(x * tileSize, y * tileSize, 0f);
+	}
+}
